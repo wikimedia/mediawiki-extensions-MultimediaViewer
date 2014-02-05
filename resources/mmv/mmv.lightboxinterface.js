3b71fecee5cf2dd9f7f033a8693458a8bb83aa7d
MarkTraceur <mtraceur@member.fsf.org>
2014-02-05 18:12:43 +0000

    Revert "Exit Lightbox when clicked on backdrop"
    This reverts commit 4280dd8b83eab0a21b0e38387289274777588c5f.
    
    After product discussion, we decided this was a bad idea. Sorry, Tony.
    
    Change-Id: Ib59d75132c2a301f83d080b0f963e2a9d8b9b277

--- resources/mmv/mmv.lightboxinterface.js
+++ resources/mmv/mmv.lightboxinterface.js
@@ -152,18 +152,12 @@
 	};
 
 	LIP.initializeInterface = function () {
-		var ui = this;
 		this.initializeHeader();
 		this.initializeNavigation();
 		this.initializeButtons();
 		this.initializeImage();
 		this.initializeImageMetadata();
 		this.initializeAboutLinks();
-		this.$imageDiv.click( function ( e ) {
-			if ( e.target.nodeName !== 'IMG' ) {
-				ui.unattach();
-			}
-		} );
 	};
 
 	LIP.initializeHeader = function () {
