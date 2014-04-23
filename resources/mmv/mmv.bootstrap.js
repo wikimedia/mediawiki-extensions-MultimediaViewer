292f42dc7ce175e53ea4851e9cd7e560869120d3
Gilles Dubuc <gdubuc@wikimedia.org>
2014-04-23 02:26:07 +0200

    Fix Chrome bug where popstate could still reset scroll position
    Change-Id: I3418b2e446a210a406daacc1321103df1f09caa1
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/486

--- resources/mmv/mmv.bootstrap.js
+++ resources/mmv/mmv.bootstrap.js
@@ -373,6 +373,8 @@
 	 * Cleans up the overlay
 	 */
 	MMVB.cleanupOverlay = function () {
+		var bootstrap = this;
+
 		$( document.body ).removeClass( 'mw-mmv-lightbox-open' );
 
 		if ( this.$overlay ) {
@@ -380,8 +382,8 @@
 		}
 
 		if ( this.savedScroll ) {
-			$.scrollTo( this.savedScroll, 0 );
-			this.savedScroll = undefined;
+			// setTimeout because otherwise Chrome will scroll back to top after the popstate event handlers run
+			setTimeout( function() { $.scrollTo( bootstrap.savedScroll, 0 ); bootstrap.savedScroll = undefined; }, 0 );
 		}
 	};
 
