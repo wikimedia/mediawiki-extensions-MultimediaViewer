b1b6ba1e1baa88243d7b9a81046c28a791932479
paladox <thomasmulhall410@yahoo.com>
2015-05-07 21:28:10 +0200

    Add extension.json, empty PHP entry point
    Bug: T87943
    Change-Id: I1251b22cf6fbeb6d31fdcf1b0662049eaacab18a

--- MultimediaViewerHooks.php
+++ MultimediaViewerHooks.php
@@ -31,6 +31,143 @@ class MultimediaViewerHooks {
 	/** Link to help about this module */
 	protected static $helpLink = '//mediawiki.org/wiki/Special:MyLanguage/Help:Extension:Media_Viewer';
 
+	public static function registerExtension() {
+		global $wgNetworkPerformanceSamplingFactor, $wgMediaViewerDurationLoggingSamplingFactor,
+		$wgMediaViewerDurationLoggingLoggedinSamplingFactor, $wgMediaViewerAttributionLoggingSamplingFactor,
+		$wgMediaViewerDimensionLoggingSamplingFactor, $wgMediaViewerActionLoggingSamplingFactorMap,
+		$wgMediaViewerIsInBeta, $wgMediaViewerUseThumbnailGuessing, $wgMediaViewerEnableByDefault,
+		$wgMediaViewerEnableByDefaultForAnonymous, $wgMediaViewerImageQueryParameter,
+		$wgMediaViewerRecordVirtualViewBeaconURI;
+		if ( !isset( $wgNetworkPerformanceSamplingFactor ) ) {
+			/** @var int|bool: If set, records image load network performance via EventLogging once per this many requests. False if unset. **/
+			$wgNetworkPerformanceSamplingFactor = false;
+		}
+
+		if ( !isset( $wgMediaViewerDurationLoggingSamplingFactor ) ) {
+			/**
+			 * If set, records loading times via EventLogging. A value of 1000 means there will be an
+			 * 1:1000 chance to log the duration event.
+			 * False if unset.
+			 * @var int|bool
+			 */
+			$wgMediaViewerDurationLoggingSamplingFactor = false;
+		}
+
+		if ( !isset( $wgMediaViewerDurationLoggingLoggedinSamplingFactor ) ) {
+			/**
+			 * If set, records loading times via EventLogging with factor specific to loggedin users.
+			 * A value of 1000 means there will be an 1:1000 chance to log the duration event.
+			 * False if unset.
+			 * @var int|bool
+			 */
+			$wgMediaViewerDurationLoggingLoggedinSamplingFactor = false;
+		}
+
+		if ( !isset( $wgMediaViewerAttributionLoggingSamplingFactor ) ) {
+			/**
+			 * If set, records whether image attribution data was available. A value of 1000 means there will be an
+			 * 1:1000 chance to log the attribution event.
+			 * False if unset.
+			 * @var int|bool
+			 */
+			$wgMediaViewerAttributionLoggingSamplingFactor = false;
+		}
+
+		if ( !isset( $wgMediaViewerDimensionLoggingSamplingFactor ) ) {
+			/**
+			 * If set, records whether image dimension data was available. A value of 1000 means there will be an
+			 * 1:1000 chance to log the dimension event.
+			 * False if unset.
+			 * @var int|bool
+			 */
+			$wgMediaViewerDimensionLoggingSamplingFactor = false;
+		}
+
+		if ( !isset( $wgMediaViewerActionLoggingSamplingFactorMap ) ) {
+			/**
+			 * If set, records user actions via EventLogging and applies a sampling factor according to the map. A "default" key in the map must be set.
+			 * False if unset.
+			 * @var array|bool
+			 */
+			$wgMediaViewerActionLoggingSamplingFactorMap = false;
+		}
+
+		if ( !isset( $wgMediaViewerIsInBeta ) ) {
+			/** @var bool: If set, Media Viewer will try to use BetaFeatures. False if unset. **/
+			$wgMediaViewerIsInBeta = false;
+		}
+
+		if ( !isset( $wgMediaViewerUseThumbnailGuessing ) ) {
+			/**
+			 * When this is enabled, MediaViewer will try to guess image URLs instead of making an
+			 * imageinfo API to get them from the server. This speeds up image loading, but will result in 404s
+			 * when $wgGenerateThumbnailOnParse (so the thumbnails are only generated as a result of the API request).
+			 * MediaViewer will catch such 404 errors and fall back to the API request, but depending on how the site
+			 * is set up, the 404 might get cached, or redirected, causing the image load to fail. The safe way to
+			 * use URL guessing is with a 404 handler: https://www.mediawiki.org/wiki/Manual:Thumb.php#404_Handler
+			 *
+			 * @var bool
+			 */
+			$wgMediaViewerUseThumbnailGuessing = false;
+		}
+
+		if ( !isset( $wgMediaViewerEnableByDefault ) ) {
+			/**
+			 * If trueish, and $wgMediaViewerIsInBeta is unset, Media Viewer will be turned on by default.
+			 * @var bool
+			 */
+			$wgMediaViewerEnableByDefault = true;
+		}
+
+		if ( !isset( $wgMediaViewerEnableByDefaultForAnonymous ) ) {
+			/**
+			 * If set, overrides $wgMediaViewerEnableByDefault for anonymous users.
+			 * @var bool
+			 */
+			$wgMediaViewerEnableByDefaultForAnonymous = $wgMediaViewerEnableByDefault;
+		}
+
+		if ( !isset( $wgMediaViewerImageQueryParameter ) ) {
+			/**
+			 * If set, adds a query parameter to image requests made by Media Viewer
+			 * @var string|bool
+			 */
+			$wgMediaViewerImageQueryParameter = false;
+		}
+
+		if ( !isset( $wgMediaViewerRecordVirtualViewBeaconURI ) ) {
+			/**
+			 * If set, records a virtual view via the provided beacon URI.
+			 * @var string|bool
+			 */
+			$wgMediaViewerRecordVirtualViewBeaconURI = false;
+		}
+	}
+
+	public static function onExtensionFunctions() {
+		global $wgResourceModules, $wgDefaultUserOptions, $wgMediaViewerEnableByDefault;
+
+		if ( isset( $wgResourceModules['ext.eventLogging'] ) ) {
+			$wgResourceModules['mmv.lightboxinterface']['dependencies'][] = 'ext.eventLogging';
+			$wgResourceModules['mmv']['dependencies'][] = 'ext.eventLogging';
+			$wgResourceModules['mmv.bootstrap.autostart']['dependencies'][] = 'ext.eventLogging';
+		}
+
+		if ( $wgMediaViewerEnableByDefault ) {
+			$wgDefaultUserOptions['multimediaviewer-enable'] = true;
+		}
+	}
+
+	public static function onEventLoggingRegisterSchemas( array &$schemas ) {
+		 $schemas += array(
+			'MediaViewer' => 10867062,
+			'MultimediaViewerNetworkPerformance' => 12458951,
+			'MultimediaViewerDuration' => 10427980,
+			'MultimediaViewerAttribution' => 9758179,
+			'MultimediaViewerDimensions' => 10014238,
+		);
+	}
+
 	/**
 	 * Checks the context for whether to load the viewer.
 	 * @param User $user
