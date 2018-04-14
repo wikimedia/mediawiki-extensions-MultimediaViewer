9351b4febdec9da218d67e1df348b7a2f87608b7
Kunal Mehta <legoktm@member.fsf.org>
2018-04-14 00:46:34 -0700

    Register configuration settings in the standard way
    There's no need to do any fancy isset() on every setting - extension.json
    will automatically take care of that.
    
    For $wgMediaViewerEnableByDefaultForAnonymous which depended on the value
    of $wgMediaViewerEnableByDefault, treat true/false as explicit values and
    null as a fallback to the other configuration setting.
    
    Keep the documentation associated with each setting in MultimediaViewer.php
    for IDEs.
    
    Change-Id: Ib34bbf75d027b8b96b2ec1a5975fa19061fadd55

--- MultimediaViewer.php
+++ MultimediaViewer.php
@@ -35,3 +35,99 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 } else {
 	die( 'This version of the MultimediaViewer extension requires MediaWiki 1.25+' );
 }
+
+// The following is for the purposes of IDEs and documentation. It is not
+// executed.
+
+/**
+ * If set, records image load network performance via
+ * EventLogging once per this many requests. False if unset.
+ *
+ * @var int|bool
+ */
+$wgMediaViewerNetworkPerformanceSamplingFactor = false;
+
+/**
+ * If set, records loading times via EventLogging. A value of 1000 means there will be an
+ * 1:1000 chance to log the duration event.
+ * False if unset.
+ * @var int|bool
+ */
+$wgMediaViewerDurationLoggingSamplingFactor = false;
+
+/**
+ * If set, records loading times via EventLogging with factor specific to loggedin users.
+ * A value of 1000 means there will be an 1:1000 chance to log the duration event.
+ * False if unset.
+ * @var int|bool
+ */
+$wgMediaViewerDurationLoggingLoggedinSamplingFactor = false;
+
+/**
+ * If set, records whether image attribution data was available.
+ * A value of 1000 means there will be an 1:1000 chance to log the attribution event.
+ * False if unset.
+ * @var int|bool
+ */
+$wgMediaViewerAttributionLoggingSamplingFactor = false;
+
+/**
+ * If set, records whether image dimension data was available.
+ * A value of 1000 means there will be an 1:1000 chance to log the dimension event.
+ * False if unset.
+ * @var int|bool
+ */
+$wgMediaViewerDimensionLoggingSamplingFactor = false;
+
+/**
+ * If set, records user actions via EventLogging and applies a sampling factor according
+ * to the map. A "default" key in the map must be set.
+ * False if unset.
+ * @var array|bool
+ */
+$wgMediaViewerActionLoggingSamplingFactorMap = false;
+
+/**
+ * If set, Media Viewer will try to use BetaFeatures. False if unset.
+ * @var bool
+ */
+$wgMediaViewerIsInBeta = false;
+
+/**
+ * When this is enabled, MediaViewer will try to guess image URLs instead of making an
+ * imageinfo API to get them from the server. This speeds up image loading, but will
+ * result in 404s when $wgGenerateThumbnailOnParse (so the thumbnails are only generated
+ * as a result of the API request). MediaViewer will catch such 404 errors and fall back
+ * to the API request, but depending on how the site is set up, the 404 might get cached,
+ * or redirected, causing the image load to fail. The safe way to use URL guessing is
+ * with a 404 handler: https://www.mediawiki.org/wiki/Manual:Thumb.php#404_Handler
+ *
+ * @var bool
+ */
+$wgMediaViewerUseThumbnailGuessing = false;
+
+/**
+ * If trueish, and $wgMediaViewerIsInBeta is unset,
+ * Media Viewer will be turned on by default.
+ * @var bool
+ */
+$wgMediaViewerEnableByDefault = true;
+
+/**
+ * Overrides $wgMediaViewerEnableByDefault for anonymous users. If
+ * set to null, will fall back to value of $wgMediaViewerEnableByDefault
+ * @var bool|null
+ */
+$wgMediaViewerEnableByDefaultForAnonymous = null;
+
+/**
+ * If set, adds a query parameter to image requests made by Media Viewer
+ * @var string|bool
+ */
+$wgMediaViewerImageQueryParameter = false;
+
+/**
+ * If set, records a virtual view via the provided beacon URI.
+ * @var string|bool
+ */
+$wgMediaViewerRecordVirtualViewBeaconURI = false;
