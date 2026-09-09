65e30a66a52c8460eb1c4c5f6544f636743beab0
Aranyap <aprum@wikimedia.org>
2026-09-09 19:35:19 +0000

    Revert^2 "Filter out non-http(s) license urls"
    This reverts commit 5053b42fba44428c6f844689ba0b9250616955bc.
    
    Reason for revert: <Reverting the revert to re-apply the fix. A corrected patch addressing the production errors has been prepared and will be deployed alongside this change>
    
    Change-Id: If4bfad36147b3f3024d8ae682cd9a569b0a2dc0a

--- resources/mmv.common/model/mmv.model.License.js
+++ resources/mmv.common/model/mmv.model.License.js
@@ -68,7 +68,16 @@ class License {
 		 *
 		 * @type {string}
 		 */
-		this.deedUrl = deedUrl;
+		this.deedUrl = undefined;
+		try {
+			// Filter out non-http(s) license urls
+			// https://phabricator.wikimedia.org/T435999
+			const url = new URL( deedUrl );
+			if ( [ 'http:', 'https:' ].includes( url.protocol ) ) {
+				this.deedUrl = deedUrl;
+			}
+		} catch ( e ) {
+		}
 
 		/**
 		 * does the author need to be attributed on reuse?
